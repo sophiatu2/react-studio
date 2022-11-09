578ae9cb18fbe6d2b7c5edeb45bdbd0176e0615d
Sophia <sophiatu217@gmail.com>
2022-11-09 16:07:52 -0500

    Finished menu

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,17 @@
 // TODO: create a component that displays a single bakery item
+import React from "react";
+import "./BakeryItem.css";
+
+export default function BakeryItem(props) {
+  const item = props.item;
+
+  return (
+    <div className="item">
+      <img src={item.image}></img>
+      <h3>{item.name}</h3>
+      <p>{item.desc}</p>
+      <p>{item.price}</p>
+      <button onClick={item.onClick}>Add to Cart</button>
+    </div>
+  );
+}
